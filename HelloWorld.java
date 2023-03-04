9a18fbe4f1fd634650f7d2331de8289fa0c0942e
LIanxj0513 <lxj990513@gmail.com>
2023-03-04 16:54:43 +0900

    DEBUGGING

--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package com.runoob.test;
-
-public class HelloWorld {
-
-}
---
+++ java/DEBUGGING/src/Debug.java
@@ -0,0 +1,15 @@
+public class Debug {
+
+  public static void main(String[] args) {
+    
+    int steps = 10;
+    
+    for (int i = 1; i <= steps; i++) {
+      
+      System.out.println("Step #" + i);
+      
+    }
+    
+  }
+  
+}
