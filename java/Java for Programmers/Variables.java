b60ff238e49ce1290ce4cd9379a10ca9fd518cb9
LIanxj0513 <lxj990513@gmail.com>
2023-03-13 10:03:38 +0900

    Test and Programmers

---
+++ java/Java for Programmers/Variables.java
@@ -0,0 +1,25 @@
+class Variables {
+  public static void main(String[] args) {
+    // int
+    int intNum = 6;
+    intNum++;
+    intNum *= 3;
+    System.out.println(intNum);
+
+    // double
+    double doubleNum;
+    doubleNum = 14.8;
+    doubleNum = 12 - 6.5;
+    doubleNum /= 2;
+    System.out.println(doubleNum);
+
+    // boolean
+    boolean a = true;
+    a = false;
+    System.out.println(a);
+
+    // String
+    String str = "Hello, world!";
+    System.out.println(str); 
+  }
+}
