22281443f707bcbf2621cbf980f84fada28ac3b8
LIanxj0513 <lxj990513@gmail.com>
2023-03-13 10:19:53 +0900

    JAVA LOOPS

---
+++ java/LEARN JAVA LOOPS/Coffee.java
@@ -0,0 +1,19 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    int cupsOfCoffee = 1;
+    
+    while (cupsOfCoffee <= 100) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      cupsOfCoffee++;
+      
+      for(int i = 1; i <= 100; i++){
+        
+      }
+    }
+    
+  }
+  
+}
